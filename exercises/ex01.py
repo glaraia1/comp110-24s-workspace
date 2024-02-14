ce0403d597dc0771675c3253b45cd118792d66fe
Gabby Laraia <glaraia1@unc.edu>
2024-02-14 02:07:04 -0500

    step 6

---
+++ exercises/ex01.py
@@ -0,0 +1,47 @@
+"""Ex01- Simple Battleship"""
+
+__author__= "730401354"
+
+BLUE_BOX: str = "\U0001F7E6"
+RED_BOX: str = "\U0001F7E5"
+WHITE_BOX: str = "\U00002B1C"
+
+p1: int= int(input("Pick a secret boat location between 1 and 4: "))
+p2: int= int(input("Guess a number between 1 and 4: "))
+
+result: str 
+boxes: str
+
+if p1 > 4:
+    print(f"Error! {p1} too high!")
+elif p1 < 1:
+    print(f"Error! {p1} too low!")
+
+
+if p2 > 4:
+    print(f"Error! {p2} too high!")
+elif p2 < 1:
+    print(f"Error! {p2} too low!")
+
+#defining the result
+if p2 == p1:
+    result = RED_BOX
+else:
+    result = WHITE_BOX
+
+#Building the blocks
+if p2 == 1:
+    print(f"{result}{BLUE_BOX * 3}")
+elif p2 == 2:
+    print(f"{BLUE_BOX}{result}{BLUE_BOX * 2}")
+
+
+if p2 == p1:
+    print(f"Correct! You hit the ship.")
+else: #p2guess1 != p1guess1
+    result = WHITE_BOX
+    print(f"Incorrect! You missed the ship.")
+
+
+
+
