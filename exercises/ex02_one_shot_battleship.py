c1eaf40f0ae4728406ee3568c3e9b3ba8c4ececc
Gabby Laraia <glaraia1@unc.edu>
2024-02-16 12:36:54 -0500

    completed

--- exercises/ex02_one_shot_battleship.py
+++ exercises/ex02_one_shot_battleship.py
@@ -2,10 +2,15 @@
 
 __author__= "730401354"
 
-grid_size: int = 5
+grid_size: int = 4
 secret_row: int = 3
-secret_column: int = 2
+secret_column: int = 2 
 
+BLUE_BOX: str = "\U0001F7E6"
+RED_BOX: str = "\U0001F7E5"
+WHITE_BOX: str = "\U00002B1C"
+
+result: str
 
 #guessing the row 
 row_guess: int = int(input("Guess a row: "))
@@ -19,6 +24,33 @@ while column_guess > grid_size or column_guess < 1: #while guess is out of bound
     try_again2: int = int(input(f"The grid is only {grid_size} by {grid_size}. Try Again: "))
     column_guess = try_again2
 
+#defining the result
+if row_guess == secret_row and column_guess == secret_column:
+    result = RED_BOX
+else:
+    result = WHITE_BOX
+
+#building the grid
+row_idx: int = 1
+while row_idx <= grid_size:
+    single_row: str = ""
+    column_idx: int = 1
+    if row_guess == row_idx:
+        while column_idx <= grid_size:
+            if column_guess == column_idx:
+                single_row += result
+            else:
+                single_row += BLUE_BOX
+            column_idx += 1 
+    else:
+        while column_idx <= grid_size:
+           single_row += BLUE_BOX
+           column_idx += 1
+    print(single_row)
+    row_idx += 1
+            
+
+#hit or miss or close
 if row_guess == secret_row and column_guess == secret_column:
     print(f"Hit!")
 else:
