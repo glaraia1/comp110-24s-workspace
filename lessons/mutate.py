2a0c2265245c665ad26f20d91233fe9dfadde760
Gabby Laraia <glaraia1@unc.edu>
2024-02-19 22:55:18 -0500

    first attempt

---
+++ lessons/mutate.py
@@ -0,0 +1,15 @@
+"""Mutating functions."""
+
+__author__ = "730401354"
+
+def manual_append(x: list[int], y: int) -> None:
+    """mutate input by appending."""
+    x.append(y)
+
+def double(z: list[int]) -> None:
+    """mutate input by multiplying every element in list 2."""
+    list_idx: int = 0
+    while list_idx < len(z)
+        z[0] *= 2
+        list_idx += 1
+        
